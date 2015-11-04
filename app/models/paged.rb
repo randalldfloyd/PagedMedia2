92eeed25dc99f7bd306f34a01f068ca94b8b61c9
Andy Smith <andjsmit@iupui.edu>
2015-11-04 16:56:23 -0500

    Added embargo to paged model. Allows new form to generate.

--- app/models/paged.rb
+++ app/models/paged.rb
@@ -4,6 +4,7 @@ class Paged < ActiveFedora::Base
   #include ::CurationConcerns::WorkBehavior
   include ::CurationConcerns::CollectionBehavior
   include ::CurationConcerns::BasicMetadata
+  include Hydra::AccessControls::Embargoable
   #include Hydra::Collection::Metadata
   validates :title, presence: { message: 'Your work must have a title.' }
 end
