55291a4a61d90f715ae811b26df54d09e24f8a3e
Andy Smith <andjsmit@iupui.edu>
2016-01-11 15:03:51 -0500

    Changed paged to work instead of collection.

--- app/models/paged.rb
+++ app/models/paged.rb
@@ -2,7 +2,7 @@
 #  `rails generate curation_concerns:work Paged`
 class Paged < ActiveFedora::Base
   #include ::CurationConcerns::WorkBehavior
-  include ::CurationConcerns::CollectionBehavior
+  include ::CurationConcerns::WorkBehavior
   include ::CurationConcerns::BasicMetadata
   #include Hydra::Collection::Metadata
   validates :title, presence: { message: 'Your work must have a title.' }
