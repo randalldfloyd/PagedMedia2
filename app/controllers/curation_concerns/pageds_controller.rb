d8a1e269c5d9164550063352fd4e61cf72decac5
Andy Smith <andjsmit@iupui.edu>
2016-01-14 12:03:22 -0500

    Members listed on paged view. Placeholder for page view.

--- app/controllers/curation_concerns/pageds_controller.rb
+++ app/controllers/curation_concerns/pageds_controller.rb
@@ -4,4 +4,26 @@
 class CurationConcerns::PagedsController < ApplicationController
   include CurationConcerns::CurationConcernController
   set_curation_concern_type Paged
+  
+  def show
+    super
+    @members = find_members()
+  end
+  
+  def find_members(*args) 
+    if args.size > 0
+      params[:id] = args[0]
+    end
+    members = {}
+    search = ActiveFedora::SolrService.instance.conn.select :params => { :q => params[:id], :fl => "member_ids_ssim" }
+    unless search['response']['numFound'].to_i == 0
+      members = search['response']['docs'][0]['member_ids_ssim']
+    else
+      members = {:id => params[:id], :error => 'No memberss'}
+    end
+    #FIXME: keep?
+    memberss ||= "[]"
+    return members
+  end
+  
 end
