2386561b29fe40182b51a40a51dc7bc57f98ff67
Andy Smith <andjsmit@iupui.edu>
2016-03-16 08:53:39 -0400

    Adds default form for adding page. Button from PAged leads to it.

--- app/controllers/curation_concerns/pages_controller.rb
+++ app/controllers/curation_concerns/pages_controller.rb
@@ -3,4 +3,9 @@
 class CurationConcerns::PagesController < ApplicationController
   include CurationConcerns::CurationConcernController
   set_curation_concern_type Page
+
+  def new
+
+  end
+
 end
---
+++ app/forms/curation_concerns/page_form.rb
@@ -0,0 +1,8 @@
+# Generated via
+#  `rails generate curation_concerns:work Paged`
+module CurationConcerns
+  class PageForm < CurationConcerns::Forms::FileSetForm
+    self.model_class = ::Page
+    self.model_name = ::Page
+  end
+end
