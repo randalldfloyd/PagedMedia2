8a7c644725f359c5fad0cd0906d3c1028c627827
Andy Smith <andjsmit@iupui.edu>
2016-04-20 09:06:47 -0400

    Fix typo.

--- app/controllers/curation_concerns/containers_controller.rb
+++ app/controllers/curation_concerns/containers_controller.rb
@@ -1,4 +1,4 @@
 class CurationConcerns::ContatinersController < ApplicationController
   include CurationConcerns::CurationConcernController
-  self.curation_concern_type = Contatiner
+  self.curation_concern_type = Container
 end
