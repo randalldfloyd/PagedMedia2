31d1de16b0bcc36d9b6d50af4c5b0d41b7288ce2
Randall Floyd <rdfloyd@indiana.edu>
2016-04-28 16:50:42 -0400

    HPT-684: Fixing spec factory for creator to be multiple as expected by latest Curation Concerns

--- spec/factories/paged_work_factories.rb
+++ spec/factories/paged_work_factories.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   #Create a paged_work object
   factory :paged_work, class: PagedWork do
     title ["Paged Work Object"]
-    creator "Factory Girl"
+    creator ["Factory Girl"]
     depositor "user@example.com"
     edit_users ["user@example.com"]
     #author "Factory Girl"
